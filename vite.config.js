29a1cb8d7c903c8f57b5a5f3419fe9c2b1862fa4
Marcelo <marcelo.desenvolvedor.contato@gmail.com>
2024-06-26 17:33:05 -0300

    fix: arrumando erro durante os imports absolutos

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/Antnest/"
+    base: "/Antnest/",
+    resolve: {
+        alias: {
+            '@tabela': path.resolve(__dirname, './src/helpers/tabela.js'),
+            '@useTabela': path.resolve(__dirname, './src/helpers/useTabela.js'),
+            '@sessao': path.resolve(__dirname, './src/helpers/sessao.js')
+        }
+    }
 })
