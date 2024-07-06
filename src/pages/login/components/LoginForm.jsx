e52f0153bc63415fbcd8a87d9838dd052cbd01a8
João Otávio Leites Matuszevski <joaoolmdev@gmail.com>
2024-07-06 16:51:08 -0300

    botãozinho novo de redirecionamento de paginas

--- src/pages/login/components/LoginForm.jsx
+++ src/pages/login/components/LoginForm.jsx
@@ -1,69 +1,74 @@
-import Tabela from "@tabela"
-import { useForm } from "react-hook-form"
-import { toast } from "sonner"
-import { vazio } from "@utils"
-import { Link, useNavigate } from "react-router-dom"
-import Sessao from "@sessao"
+import Tabela from "@tabela";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { vazio } from "@utils";
+import { Link, useNavigate } from "react-router-dom";
+import Sessao from "@sessao";
 
-export default function LoginForm(){
-    const {handleSubmit, register} = useForm()
+export default function LoginForm() {
+  const { handleSubmit, register } = useForm();
 
-    const navegar = useNavigate()
+  const navegar = useNavigate();
 
-    /** @param {{senha: string, email: string}} data*/
-    function logar(data){
-        /** @type {Tabela}*/
-        const usuarios = new Tabela("usuario")
+  /** @param {{senha: string, email: string}} data*/
+  function logar(data) {
+    /** @type {Tabela}*/
+    const usuarios = new Tabela("usuario");
 
-        /** @type {Usuario}*/
-        const usuario = usuarios.encontrarUmPor("email", data.email)
+    /** @type {Usuario}*/
+    const usuario = usuarios.encontrarUmPor("email", data.email);
 
-        if(vazio(usuario)){
-            toast.error("Ops! E-mail incorreto :(") 
-            return
-        } 
-        else if (usuario.senha != data.senha){
-            toast.error("Ops! Senha errada :(") 
-            return
-        }
-
-        Sessao.criar(usuario.id)
-        toast.success("Autenticado com sucesso :)", {position: "top-center"})
-
-        navegar("/")
+    if (vazio(usuario)) {
+      toast.error("Ops! E-mail incorreto :(");
+      return;
+    } else if (usuario.senha != data.senha) {
+      toast.error("Ops! Senha errada :(");
+      return;
     }
 
-    return(
+    Sessao.criar(usuario.id);
+    toast.success("Autenticado com sucesso :)", { position: "top-center" });
 
-        <>
-            <main className="flex justify-center items-center w-screen h-screen">
-                <form action="input" onSubmit={handleSubmit(logar)}>
-                    <h1 className="font-bold text-xl">
-                        Login
-                    </h1>
-                    <div className="flex flex-col bg-primary gap-8 p-8 rounded-xl justify-between">
+    navegar("/");
+  }
 
-                        <label htmlFor="email" className="flex justify-between" >
-                            <input 
-                                className="rounded" 
-                                type="email" 
-                                placeholder=" E-mail" 
-                                {...register("email", {required: true})}/> 
-                        </label>
-                        <label htmlFor="senha" className="flex justify-between" >
-                            <input 
-                                type="password" 
-                                className="rounded" 
-                                placeholder=" Senha" 
-                                {...register("senha", {required: true})}/>  
-                        </label>
+  return (
+    <>
+      <main className="flex justify-center items-center w-screen h-screen">
+        <form action="input" onSubmit={handleSubmit(logar)}>
+          <h1 className="font-bold text-xl">Login</h1>
+          <div className="flex flex-col bg-primary gap-8 p-8 rounded-xl justify-between">
+            <label htmlFor="email" className="flex justify-between">
+              <input
+                className="rounded"
+                type="email"
+                placeholder=" E-mail"
+                {...register("email", { required: true })}
+              />
+            </label>
+            <label htmlFor="senha" className="flex justify-between">
+              <input
+                type="password"
+                className="rounded"
+                placeholder=" Senha"
+                {...register("senha", { required: true })}
+              />
+            </label>
 
-                        <input type="submit" value="Fazer Login" className="text-primary bg-secondary rounded"/>
-                    </div>
-                    <Link to="/registro">Ir para Registro</Link>
-                </form>
-            </main>
-        </>
-        
-    )
+            <input
+              type="submit"
+              value="Fazer Login"
+              className="text-text bg-secondary rounded"
+            />
+          </div>
+          <div className="">
+            <p className="font-bold">Não possue conta? </p>
+            <button className="bg-secondary rounded-md p-1">
+              <Link to="/registro">Registre-se aqui</Link>
+            </button>
+          </div>
+        </form>
+      </main>
+    </>
+  );
 }
--- src/pages/registro/components/RegistroForm.jsx
+++ src/pages/registro/components/RegistroForm.jsx
@@ -1,90 +1,100 @@
-import Tabela from "@tabela"
-import { useForm } from "react-hook-form"
-import { toast } from "sonner"
-import { vazio } from "@utils"
-import { Link, useNavigate } from "react-router-dom"
+import Tabela from "@tabela";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { vazio } from "@utils";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function RegistroForm(){
-    const {handleSubmit, register} = useForm()
+export default function RegistroForm() {
+  const { handleSubmit, register } = useForm();
 
-    const navegar = useNavigate()
+  const navegar = useNavigate();
 
-    /** @param {{nome: string, senha: string, email: string, confsenha: string}} data*/
-    function registrar(data){
-        if (data.confsenha != data.senha){
-            toast.error("As senhas estão diferentes :(", {position: "top-center"}) 
-            return
-        }
-
-        /** @type {Tabela}*/
-        const usuarios = new Tabela("usuario")
-
-        if(!vazio(usuarios.encontrarUmPor("email", data.email))){
-            toast.error("Desculpe, esse E-mail já está em uso :(") 
-            return
-        }
-
-        /** @type {Usuario}*/
-        const usuario = {
-            id: crypto.randomUUID(),
-            nome: data.nome,
-            email: data.email,
-            senha: data.senha,
-            urlImagem: "/public/fotoUsuarioPlaceholder.jpg"
-        } 
-
-        usuarios.adicionar(usuario)
-        usuarios.enviarParaLocalStorage()
-
-        toast.success("Sucesso! Bem-vindo ao AntNest :)", {position: "top-center"})
-        navegar("/login")
+  /** @param {{nome: string, senha: string, email: string, confsenha: string}} data*/
+  function registrar(data) {
+    if (data.confsenha != data.senha) {
+      toast.error("As senhas estão diferentes :(", { position: "top-center" });
+      return;
     }
 
-    return(
+    /** @type {Tabela}*/
+    const usuarios = new Tabela("usuario");
 
-        <>
-            <main className="flex justify-center items-center w-screen h-screen">
-                <form action="input" onSubmit={handleSubmit(registrar)}>
-                    <h1 className="font-bold text-xl">
-                        Registro
-                    </h1>
-                    <div className="flex flex-col bg-primary gap-8 p-8 rounded-xl justify-between">
-
-                        <label htmlFor="nome" className="flex justify-between" >
-                            <input 
-                                className="rounded" 
-                                type="text" 
-                                placeholder=" Nome" 
-                                {...register("nome", {required: true})}/> 
-                        </label>
-                        <label htmlFor="email" className="flex justify-between" >
-                            <input 
-                                className="rounded" 
-                                type="email" 
-                                placeholder=" E-mail" 
-                                {...register("email", {required: true})}/> 
-                        </label>
-                        <label htmlFor="senha" className="flex justify-between" >
-                            <input 
-                                type="password" 
-                                className="rounded" 
-                                placeholder=" Senha" 
-                                {...register("senha", {required: true})}/>  
-                        </label>
-                        <label htmlFor="confsenha" className="flex justify-between" >
-                            <input 
-                                type="password" 
-                                className="rounded" 
-                                placeholder=" Confirmar Senha" 
-                                {...register("confsenha", {required: true})}/> 
-                        </label>
-
-                        <input type="submit" value="registrar-se" className="text-primary bg-secondary rounded"/>
-                    </div>
-                    <Link to="/login">Ir Para Login</Link>
-                </form>
-            </main>
-        </>
-        
-    )
+    if (!vazio(usuarios.encontrarUmPor("email", data.email))) {
+      toast.error("Desculpe, esse E-mail já está em uso :(");
+      return;
     }
+
+    /** @type {Usuario}*/
+    const usuario = {
+      id: crypto.randomUUID(),
+      nome: data.nome,
+      email: data.email,
+      senha: data.senha,
+      urlImagem: "/public/fotoUsuarioPlaceholder.jpg",
+    };
+
+    usuarios.adicionar(usuario);
+    usuarios.enviarParaLocalStorage();
+
+    toast.success("Sucesso! Bem-vindo ao AntNest :)", {
+      position: "top-center",
+    });
+    navegar("/login");
+  }
+
+  return (
+    <>
+      <main className="flex justify-center items-center w-screen h-screen">
+        <form action="input" onSubmit={handleSubmit(registrar)}>
+          <h1 className="font-bold text-xl">Registro</h1>
+          <div className="flex flex-col bg-primary gap-8 p-8 rounded-xl justify-between">
+            <label htmlFor="nome" className="flex justify-between">
+              <input
+                className="rounded"
+                type="text"
+                placeholder=" Nome"
+                {...register("nome", { required: true })}
+              />
+            </label>
+            <label htmlFor="email" className="flex justify-between">
+              <input
+                className="rounded"
+                type="email"
+                placeholder=" E-mail"
+                {...register("email", { required: true })}
+              />
+            </label>
+            <label htmlFor="senha" className="flex justify-between">
+              <input
+                type="password"
+                className="rounded"
+                placeholder=" Senha"
+                {...register("senha", { required: true })}
+              />
+            </label>
+            <label htmlFor="confsenha" className="flex justify-between">
+              <input
+                type="password"
+                className="rounded"
+                placeholder=" Confirmar Senha"
+                {...register("confsenha", { required: true })}
+              />
+            </label>
+
+            <input
+              type="submit"
+              value="registrar-se"
+              className="text-primary bg-secondary rounded"
+            />
+          </div>
+          <div className="">
+            <p className="font-bold">Já possue conta?</p>
+            <button className="bg-secondary rounded-md p-1">
+              <Link to="/login">Faça login aqui</Link>
+            </button>
+          </div>
+        </form>
+      </main>
+    </>
+  );
+}
